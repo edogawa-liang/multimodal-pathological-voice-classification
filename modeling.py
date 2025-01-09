9a06aa154117a249845cde38452351e85c79c7da
edogawa-liang <tiffany217.h@gmail.com>
2025-01-09 22:05:51 +0800

    add report

--- modeling.py
+++ modeling.py
@@ -27,16 +27,27 @@ def train_model(model_name, X_train, y_train, X_test, y_test):
 
     # Initialize the model based on model_name
     if model_name == "SVM":
-        model = SVC(kernel='rbf', C=1.0, probability=True, random_state=42)
+        model = SVC(kernel='rbf', C=1.0, probability=True, random_state=42, class_weight='balanced')
     elif model_name == "XGB":
-        model = XGBClassifier(objective='multi:softmax', num_class=len(set(y_train)), random_state=42)
+        model = XGBClassifier(objective='multi:softmax', num_class=len(set(y_train)), random_state=42, class_weight='balanced')
     elif model_name == "LGBM":
-        model = LGBMClassifier(objective='multiclass', num_class=len(set(y_train)), random_state=42)
+        model = LGBMClassifier(objective='multiclass', num_class=len(set(y_train)), random_state=42, is_unbalance=True)
     elif model_name == "Logistic":
-        model = LogisticRegression(solver='lbfgs', multi_class='multinomial', random_state=42)
+        model = LogisticRegression(solver='lbfgs', multi_class='multinomial', random_state=42, class_weight='balanced')
     else:
         raise ValueError("Invalid model_name. Choose from 'SVM', 'XGB', 'LGBM', or 'Logistic'.")
 
+    # if model_name == "SVM":
+    #     model = SVC(kernel='rbf', C=1.0, probability=True, random_state=42)
+    # elif model_name == "XGB":
+    #     model = XGBClassifier(objective='multi:softmax', num_class=len(set(y_train)), random_state=42)
+    # elif model_name == "LGBM":
+    #     model = LGBMClassifier(objective='multiclass', num_class=len(set(y_train)), random_state=42)
+    # elif model_name == "Logistic":
+    #     model = LogisticRegression(solver='lbfgs', multi_class='multinomial', random_state=42)
+    # else:
+    #     raise ValueError("Invalid model_name. Choose from 'SVM', 'XGB', 'LGBM', or 'Logistic'.")
+    
     # Train the model
     model.fit(X_train, y_train)
     
@@ -45,12 +56,12 @@ def train_model(model_name, X_train, y_train, X_test, y_test):
 
     # Calculate evaluation metrics
     accuracy = accuracy_score(y_test, y_pred)
-    # precision = precision_score(y_test, y_pred, average='macro')
-    # recall = recall_score(y_test, y_pred, average='macro') # UAR
-    # f1 = f1_score(y_test, y_pred, average='macro')
-    precision = precision_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted') 
-    f1 = f1_score(y_test, y_pred, average='weighted')
+    precision = precision_score(y_test, y_pred, average='macro')
+    recall = recall_score(y_test, y_pred, average='macro') # UAR
+    f1 = f1_score(y_test, y_pred, average='macro')
+    # precision = precision_score(y_test, y_pred, average='weighted')
+    # recall = recall_score(y_test, y_pred, average='weighted') 
+    # f1 = f1_score(y_test, y_pred, average='weighted')
 
     # Print metrics for visibility
     print(f"{model_name} Performance:")
